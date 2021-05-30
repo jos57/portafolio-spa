8cfb8decf38b696354c216a52596b6f3595df076
jos57 <josue_ojeda88@hotmail.com>
2021-05-30 11:59:57 -0300

    optimized form

--- src/Contacto.js
+++ src/Contacto.js
@@ -7,22 +7,23 @@ const Contacto = () => {
 
                 <h1 className="text-center text-dark p-lg-4 pt-md-5 mt-md-5 shadow-lg">Contacto</h1>
 
-                <form className=" py-5 ">
+                <form className=" py-5 " name="contact" method="POST">
+                <input type="hidden" name="form-name" value="contact" />
                         <div className="container">
                                 <div className="offset-lg-2 col-lg-8 offset-lg-2">
                                 <div className="row ">
                                             <div className="form-group col-lg-6">
-                                                <input placeholder="Nombre" name="name" type="text" className="form-control" />
+                                                <input placeholder="Nombre" name="name" type="text" className="form-control" required />
                                             </div>
                                             <div className="form-group col-lg-6 ">
-                                                <input placeholder="Email" name="email" type="email" className="form-control"/>
+                                                <input placeholder="Email" name="email" type="email" className="form-control" required/>
                                             </div>
                                 </div>
                                 <div className=" form-group">
                                         <textarea placeholder="Escribe tu mensaje, por favor" className="col-lg-12 form-control" rows="8"></textarea>
                                 </div>
                                 <div className="row d-flex justify-content-center">
-                                    <div className="btn btn-primary col-lg-6 py-3">Enviar</div>
+                                    <input className="btn btn-primary col-lg-6 py-3" type="submit" value="Enviar" />
                                 </div>
                             </div>
                         </div>
