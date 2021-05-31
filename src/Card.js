bd0a7cb81ec15fd38373f2b30f964fbf5108afad
jos57 <josue_ojeda88@hotmail.com>
2021-05-31 15:23:58 -0300

    images

--- src/Card.js
+++ src/Card.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import head from './head(phone).jpg';
 
 
 
 
-const Card = () => {
+
+
+
+const Card = (props) => {
+
+    const  { card } = props;
+
+
     return  <div className=" row col-lg-3 m-lg-1 bg-dark shadow-lg p-0 d-flex d-lg-inline mb-md-2 mb-4">
-
-                   
-                        
+                
         <div className="card-header m-0 col-lg-12 col-md-6 col-sm-6">
-            <img src={head}  alt= "imagen-fondo-cabecera" className=" mt-2 img-fluid"></img>
+            <img src={ card.img }  alt= {card.copy} className=" mt-2 img-fluid"/>
         </div>
 
 
     <div className="card-body d-flex flex-column col-sm-6 col-md-6 col-lg-12">
 
-        <div className="card-title h3 text-center pb-3">Landing Page</div>
+        <div className="card-title h3 text-center pb-3">{card.title}</div>
         
         <div className="btn btn-info text-white btn-block">Ver Página</div>
 
--- src/conocimientos.js
+++ src/conocimientos.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import Card from './Card.js';
+import Card from './Card.js'; 
+import proyectos from '/home/joss/my-landingPage/my-app01/src/proyectos.json'
 
 
 
 
 const CardDiv = () => {
+
+    
+
     return <div id="proyectos" className="alturita">
 
         
         <div className="container-fluid row align-items-start justify-content-around m-0" style={{ paddingTop:100, paddingBottom:100}}>
-            <Card />
-            <Card />
-            <Card />
+            { 
+               proyectos.map(card => (<Card key= { card.id } card = { card } />))
+            }
+           
         </div>
+        
 
-
+      
 
     </div>
 }
