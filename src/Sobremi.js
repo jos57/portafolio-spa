0ae40e93f9445d8a9869cbe63b3874532b32b0a0
jos57 <josue_ojeda88@hotmail.com>
2021-06-08 16:49:57 -0300

    borrando carpetas fuera de src

--- src/Sobremi.js
+++ src/Sobremi.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import img from '/home/joss/my-landingPage/my-app01/src/images/escalando.jpg';
+import img from '/home/joss/my-landingPage/my-app01/src/escalando.jpg';
 
 
 
